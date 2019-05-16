8d5aca17e27a21da21bba6c14799be19408ae671
John David Martin <johndavidfive@gmail.com>
2019-05-16 10:52:02 -0700

    updating the index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,50 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        htmlTitle: `Netlify CMS Demo`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/_posts/pages/`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `books`,
+    //     path: `${__dirname}/_posts/books/`,
+    //     ignore: [`**/\.*`], // ignore files starting with a dot
+    //   },
+    // },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [
+          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {},
+          }
+        ],
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,49 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+import "./index.css";
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+
+    const Posts = edges.map(edge => {
+
+        return (
+            <div className="demo-page">
+                <h3>{edge.node.frontmatter.title}{JSON.stringify(edge, null, 2)}</h3>
+                <div dangerouslySetInnerHTML={{__html:edge.node.excerpt}}></div>
+                <img width={200} alt="awesome" src={edge.node.frontmatter.thumbnail}/>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            <h2>Pages</h2>
+            <div>{Posts}</div>
+        </div>
+
+    );
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            thumbnail
+          }
+          excerpt(format: HTML)
+        }
+      }
+    }
+  }
+`
